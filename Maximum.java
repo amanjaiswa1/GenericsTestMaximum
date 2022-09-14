99229e190580799fafbc77d5a0c0f0189fcb7ea3
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-09-15 00:05:15 +0530

    Modify: Taken More Then Three Parameters

--- Maximum.java
+++ Maximum.java
@@ -1,40 +1,65 @@
 package com.assignments.day13.Generics;
 
-public class Maximum<T extends Comparable<T>> {
-	 T x,y,z;
+import java.util.*;
 
-	    public Maximum(T x,T y,T z) {
-	        this.x = x;
-	        this.y = y;
-	        this.z = z;
+public class Maximum<T extends Comparable<T>> {
+	 T a,b,c,d,e;
+	 List<T> list = new ArrayList<T>();
+	    public void sortList() {
+	        Maximum max = new Maximum(1, 2, 3, 4, 5);
+	        max.list.add(this.a);
+	        max.list.add(this.b);
+	        max.list.add(this.c);
+	        max.list.add(this.d);
+	        max.list.add(this.e);
+
+	        Collections.sort(max.list);
+	        System.out.println(max.list);
+	    }
+
+	    public Maximum(T a,T b,T c,T d,T e) {
+	        this.a = a;
+	        this.b = b;
+	        this.c = c;
+	        this.d = d;
+	        this.e = e;
 	    }
 	    public T maximum() {
-	        return Maximum.maximum(x,y,z);
+	        return Maximum.maximum(a,b,c,d,e);
 	    }
-	    public static <T extends Comparable<T>> T maximum(T x,T y,T z) {
-	        T max = x;
-	        if (y.compareTo(max) > 0) {
-	            max = y;
+	    public static <T extends Comparable<T>> T maximum(T a,T b,T c,T d,T e) {
+	        T max = a;
+	        if (b.compareTo(max) > 0) {
+	            max = b;
 	        }
-	        if (z.compareTo(max) > 0) {
-	            max = z;
+	        if (c.compareTo(max) > 0) {
+	            max = c;
 	        }
-	        printMax(x,y,z,max);
+	        if (d.compareTo(max) > 0) {
+	            max = d;
+	        }
+	        if (e.compareTo(max) > 0) {
+	            max = e;
+	        }
+	        printMax(a,b,c,d,e,max);
 	        return max;
 	    }
-	    public static <T> void printMax(T x,T y,T z,T max) {
-	        System.out.println("Max of "+x+", "+y+" and "+z+" is : "+max+"\n");
+	    public static <T> void printMax(T a,T b,T c,T d,T e,T max) {
+	        System.out.println("\n"+"Max of " + a + ", " + b + ", " + c + ", " + d + " and " + e + " is : "+max);
 	    }
 
   public static void main(String[] args) {
-	  System.out.println("Test Case 1: Max String At 1st Position.");
-	  Maximum<String> m1=new Maximum<String>("Peach","Apple","Banana");
+	  
+	  Maximum<Integer> m1=new Maximum<Integer>(8,9,12,4,6);
 	  m1.maximum();
-	  System.out.println("Test Case 2: Max String At 2nd Position.");
-	  Maximum<String> m2=new Maximum<String>("Apple","Peach","Banana");
+	  m1.sortList();
+	  
+	  Maximum<Float> m2=new Maximum<Float>(10.1f,20.2f,5.5f,6.6f,30.3f);
 	  m2.maximum();
-	  System.out.println("Test Case 3: Max String At 3rd Position.");
-	  Maximum<String> m3=new Maximum<String>("Apple","Banana","Peach");
+	  m2.sortList();
+	  
+	  Maximum<String> m3=new Maximum<String>("Apple","Banana","Peach","Mango","Grape");
 	  m3.maximum();
+	  m3.sortList();
   }
 }
